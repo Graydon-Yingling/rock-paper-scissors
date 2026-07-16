c5dcb6dc99b53c1e83fa465d95ef07598b9f1ac9
Graydon <gyingling2024@gmail.com>
2026-07-16 18:56:57 -0400

    Implemented computer input to Rock Paper Scissors game

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello, World!");
+
+
+function getComputerChoice() {
+    let rand = Math.random();
+
+    if (rand > 0 && rand < 1/3) {
+        return("Rock");
+    }else if (rand >= 1/3 && rand < 2/3) {
+        return("Paper");
+    }else {
+        return("Scissors");
+    };
+};
